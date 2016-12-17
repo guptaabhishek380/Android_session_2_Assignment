644fa1fcf5fdc9ad8b76a78e0394b5d1710999e0
ABHISHEK\user <gupta.abhi380@gmail.com>
2016-12-17 15:04:23 +0530

    Android Session 2 Assignment(2.1,2.2,2.3)

---
+++ assignment_2.2/credit_card/app/src/main/java/com/abhi380/gupta/credit_card/MainActivity.java
@@ -0,0 +1,14 @@
+package com.abhi380.gupta.credit_card;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
